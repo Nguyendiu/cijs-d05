42a83f928eaf682066cccbda264366b3f39e020d
Nguyendiu <nguyendiupro15@gmail.com>
2020-11-28 19:14:28 +0700

    save

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import {data} from '../Data/data'
+import { data } from '../Data/data'
 export function SearchBar(props) {
+    // handleKeyPress(e){
+    //     if (e.key === 'Enter') {
+    //         this.Search();
 
+    //     }
 
-    return (
-        <>
-        <input onChange={props.onChange} className="full"/>
-        <div className='result'>
-        <img src = {data.projects.project.[0].imageLink} id='img'/>
-        <h3> {data.projects.project.[0].contactName}</h3>
-        <a href={data.projects.project.[0].projectLink}> {data.projects.project.[0].projectLink} </a>
-       </div> 
-       </>
-    )
+        return (
+            <>
+                <input onChange={props.onChange} className="full" />
+                <button>Search</button>
+                <div className='result' key="">
+                    <img src={data.projects.project.[0].imageLink} id='img' />
+                    <h3> {data.projects.project.[0].contactName}</h3>
+                    <a href={data.projects.project.[0].projectLink}> {data.projects.project.[0].projectLink} </a>
+                </div>
+            </>
+        )
 
-}
+    }
