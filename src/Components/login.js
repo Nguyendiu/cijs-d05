0ee0385e1cb93967092f32134a9d658ab0a02472
Nguyendiu <nguyendiupro15@gmail.com>
2020-12-13 13:28:42 +0700

    s

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
+import firebase from 'firebase'
+import { firebaseConfig } from '../firebase/firebaseConfig'
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (e) {
+  console.log(e.message)
+}
+
+
+const db = firebase.firestore();
 export function Formlogin({Login,error}) {
     const [details,setDetails] = useState({name:"",email:"", password: ''});
+    db.collection("userName").add({
+        email:details.name,
+        passWord:details.password,
+        email: details.email
+    })
     const submitHanler = (e)=>{
         e.preventDefault();
         Login(details);
       
     }
     const inputHanler= (e) =>{
+        
         setDetails({...details,name:e.target.value})
     }
     const emailHanler = (e) =>{
+       
         setDetails({...details,email:e.target.value})
     }
     const passwordHanler = e =>{
+      
         setDetails({...details,password:e.target.value})
     }
+    console.log(details.name)
     return (
+        <div>
         <form className="Login" onSubmit={submitHanler}>
            <p>Login</p>
            {/*ERROR!*/}
@@ -24,7 +44,12 @@ export function Formlogin({Login,error}) {
            <input type='password' onChange={passwordHanler} placeholder='Pass....'></input> <br></br>
            <button>Login</button>
            
+           
         </form>
+        <div>
+        <button>Register</button>
+        </div>
+        </div>
     );  
 }
 export default Formlogin;
