9ffeb46ec64c7bd48ff2a4c128d2ceeacf2f5b1b
Nguyendiu <nguyendiupro15@gmail.com>
2021-02-11 14:49:03 +0700

    xong

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+class App extends React.Component {
+  state = {
+    name: 'circle'
+  }
+  startRoll = () => {
+    this.setState({
+      name: 'circle start-rotate'
+    });
+    setTimeout(() => {
+      this.setState({
+        name: 'circle start-rotate stop-rotate'
+      })
+    }, Math.floor(Math.random() * 20000) + 1)
 
-function App() {
-  return (
-    <div className="App">
-       <p>hello</p>
-    </div>
-  );
+  }
+  render() {
+    return (
+      <div>
+        <div className='arrow'></div>
+        <ul className={this.state.name}>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>10k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>20k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>10k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>mất lượt</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>20k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>10k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>0</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>100k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>mất lượt</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>50k</div>
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>10k</div>
+
+          </li>
+          <li>
+            <div className='text' contentEditable='true' spellCheck='false'>20k</div>
+          </li>
+
+        </ul>
+        <button className='roll' onClick={this.startRoll}>Quay</button>
+      </div>
+    )
+  }
 }
 
 export default App;
